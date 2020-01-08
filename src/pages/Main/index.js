d58056ab490f2bc4914b8e1ae583440e3bd3535f
Shilton <shilton.sistemas@gmail.com>
2020-01-08 13:33:14 -0300

    Setting Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
-    return <View></View>;
+    return <Container />;
 }
 
 Main.navigationOptions = {
