34eb2abc63b255e064f7516c401e756c3068c18e
Shilton <shilton.sistemas@gmail.com>
2020-01-08 14:16:06 -0300

    Creating form and layout adjustment

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-    return <Container />;
+    return (
+        <Container>
+            <Form>
+                <Input
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Informe o usuário"
+                />
+
+                <SubmitButton>
+                    <Icon name="person-add" size={20} color="#FFF" />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
 
 Main.navigationOptions = {
